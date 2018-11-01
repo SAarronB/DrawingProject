2ba4a912f5ce751d241114508e2e77ca46a8ac6f
SAaronB <programmingsean@gmail.com>
2018-11-01 08:25:45 -0600

    Initial commit

---
+++ DrawingProject/New Group/DrawingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingProject
+//
+//  Created by Bonilla, Sean on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class DrawingViewController: UIViewController {
+
+    override func viewDidLoad() -> Void {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
